1a4e237efde93e3da7e7dc43f28f7eb42f824d4a
Thijs Dickmans <thijsdickmans@gmail.com>
2015-09-05 02:05:12 +0200

    Finished styling

--- app/app.js
+++ app/app.js
@@ -1,3 +1,12 @@
 'use-strict'
 
 var app = angular.module('better-vbl', []);
+
+// Heren
+// http://vblcb.wisseq.eu/VBLCB_WebService/data/matchesbyteamguid?teamGuid=BVBL1281HSE++1
+
+// Reserven
+// http://vblcb.wisseq.eu/VBLCB_WebService/data/matchesbyteamguid?teamGuid=BVBL1281HSE++2
+
+// Dames
+// http://vblcb.wisseq.eu/VBLCB_WebService/data/matchesbyteamguid?teamGuid=BVBL1281DSE++1
